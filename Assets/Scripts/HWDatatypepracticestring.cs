078f8b2a2b9122d0c6d877e03b1d6e5890b30e9b
julijemart <julienmartin@lcad.edu>
2024-09-03 23:35:45 -0700

    Update HWDatatypepracticestring.cs

--- Assets/Scripts/HWDatatypepracticestring.cs
+++ Assets/Scripts/HWDatatypepracticestring.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class HWDatatypepracticestring : MonoBehaviour
 {
     public string rupaul = "she done already done had herses";
+    public string rupaul2 = "now sissy that walk";
+    public string rupaul3 = "shantay you stay";
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(rupaul);
+        Debug.Log(rupaul2);
+        Debug.Log(rupaul3);
     }
 
     // Update is called once per frame
