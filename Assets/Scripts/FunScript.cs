a8f366be96cd70d54984f130f7d10206784d0c74
julijemart <julienmartin@lcad.edu>
2024-09-10 14:07:08 -0700

    WEEK3

---
+++ Assets/Scripts/FunScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void charlixcx()
+    {
+        Debug.Log("when i go to the club i wanna hear those club classics");
+    }
+}
---
+++ Assets/Scripts/RefScript.cs
@@ -0,0 +1,29 @@
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RefScript : MonoBehaviour
+{
+    public Light lightref;
+    public GameObject itscharlibaby;
+    public FunScript partygirl;
+    public Image smilexcx;
+
+    // St,M  art is called before the first frame update
+    void Start()
+    {
+        partygirl.charlixcx();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
